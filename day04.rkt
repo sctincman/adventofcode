a7412879369ef2a9bbf624ffc5ed25211d11fd23
Jonathan Tinkham <sctincman@gmail.com>
2016-03-02 22:21:55 -0700

    Day 4: cleanup excess lambda and let

--- day04.rkt
+++ day04.rkt
@@ -6,22 +6,20 @@
 
 ;Part 1
 (let loop [(value 1)]
-  (let [(hash (call-with-input-string (string-append salt
-						     (number->string value))
-		(lambda (in)
-		  (md5 in))))]
-    (if (equal? "00000"
-		(substring hash 0 5))
-	value
-	(loop (+ 1 value)))))
+  (if (equal? "00000"
+	      (substring (call-with-input-string
+			     (string-append salt
+					    (number->string value))
+			   md5) 0 5))
+      value
+      (loop (+ 1 value))))
 
 ;Part 2
 (let loop [(value 1)]
-  (let [(hash (call-with-input-string (string-append salt
-						     (number->string value))
-		(lambda (in)
-		  (md5 in))))]
-    (if (equal? "000000"
-		(substring hash 0 6))
-	value
-	(loop (+ 1 value)))))
+  (if (equal? "000000"
+	      (substring (call-with-input-string
+			     (string-append salt
+					    (number->string value))
+			   md5) 0 6))
+      value
+      (loop (+ 1 value))))
